1bb9fcadc1771dd4f5348a78fa690b999fa3a3f3
ruxnrosa <85415541+ruxnrosa@users.noreply.github.com>
2026-09-21 21:24:36 -0300

    aa

--- automation/useAITransferHandler.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-async function aiTransferHandler(params) {
-  return { success: false, message: "Ai plugin is required" };
-}
-
-module.exports = { aiTransferHandler };
