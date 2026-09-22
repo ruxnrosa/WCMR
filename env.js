40f29af711dc556cf51dfdd6ed106bb8ca74c4da
ruxnrosa <85415541+ruxnrosa@users.noreply.github.com>
2026-09-21 21:32:17 -0300

    abbb

--- env.js
+++
@@ -1,4 +0,0 @@
-const appVersion = "5.7";
-const addON = [""];
-
-module.exports = { appVersion, addON };
--- helper/addon/wacall/wacall.js
+++
@@ -1,9 +0,0 @@
-function handleCalls() {
-  return;
-}
-
-function checkWaCall() {
-  return false;
-}
-
-module.exports = { handleCalls, checkWaCall };
